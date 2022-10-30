81359a162e16636a822ff1466b4a75e7190c9e93
Miro Adamy <miro.adamy@gmail.com>
2022-10-30 13:18:40 +0000

    Created instance

--- ec2-lab-2/main.tf
+++ ec2-lab-2/main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "eu-west-2"
 }
 
 #Create an EC2 Resource Block
+resource "aws_instance" "example" {
+    ami = "ami-0648ea225c13e0729"
+    instance_type = "t2.micro"
+
+  
+}
