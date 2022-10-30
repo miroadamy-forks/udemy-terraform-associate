351f0cae116cd01d82027e2b302b2e6ba8c37516
Miro Adamy <miro.adamy@gmail.com>
2022-10-30 13:31:09 +0000

    Created - label 'name'

--- ec2-lab-3/main.tf
+++ ec2-lab-3/main.tf
@@ -1,12 +1,14 @@
-#Provider Block
 provider "aws" {
-    region = "us-east-1"
+    region = "eu-west-2"
 }
 
-#EC2 Block w/tags
-resource "aws_instance" "tags-test" {
-    ami = "ami-0947d2ba12ee1ff75"
+#Create an EC2 Resource Block
+resource "aws_instance" "example" {
+    ami = "ami-0648ea225c13e0729"
     instance_type = "t2.micro"
-    #Add Tags
 
+    tags = {
+      "name" = "Example Instance"
+      "date" = "2022-10-30_14:23"
+    }
 }
